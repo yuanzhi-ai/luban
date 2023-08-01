4f8403a0ebfa4677038aa24c7d87c527b1011d00
MJYuan <474354860@qq.com>
2023-08-01 23:03:21 +0800

    添加短信验证码

--- server/sms/sendCaptchaSms.go
+++ server/sms/sendCaptchaSms.go
@@ -28,14 +28,13 @@ func createClient(accessKeyId *string, accessKeySecret *string) (_result *dysmsa
 
 // 发送短信验证码
 func SendMsg(code string, phoneNumber string) error {
-	return nil
 	client, err := createClient(tea.String(os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_ID")), tea.String(os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET")))
 	if err != nil {
 		return err
 	}
 	sendSmsRequest := &dysmsapi20170525.SendSmsRequest{
-		SignName:      tea.String("验证码短信"),
-		TemplateCode:  tea.String("SMS_282660247"),
+		SignName:      tea.String("万象绘验证码短信"),
+		TemplateCode:  tea.String("SMS_462225349"),
 		PhoneNumbers:  tea.String(phoneNumber),
 		TemplateParam: tea.String(fmt.Sprintf("{\"code\":\"{%v}\"}", code)),
 	}
